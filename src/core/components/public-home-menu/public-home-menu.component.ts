d4a393dc10a126717a9288d34e1b89be75d0cbb7
Oleksandr Kirpachov <o.kirpachov@opinioni.net>
2024-07-20 18:00:40 +0200

    NEW landing page menu section draft

--- src/core/components/public-home-menu/public-home-menu.component.ts
+++ src/core/components/public-home-menu/public-home-menu.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TuiButtonModule} from "@taiga-ui/core";
+import {RouterLink} from "@angular/router";
+import {TuiCarouselModule} from "@taiga-ui/kit";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-public-home-menu',
   standalone: true,
-  imports: [],
+  imports: [
+    TuiButtonModule,
+    RouterLink,
+    TuiCarouselModule,
+    NgForOf
+  ],
   templateUrl: './public-home-menu.component.html',
   styleUrl: './public-home-menu.component.scss'
 })
 export class PublicHomeMenuComponent {
+  index = 0;
 
+  readonly items = [
+    'public-home-menu-photo1.webp',
+    'public-home-menu-photo2.webp',
+    'public-home-menu-photo3.webp',
+    'public-home-menu-photo4.webp',
+    'public-home-menu-photo5.webp',
+    'public-home-menu-photo6.webp',
+  ];
 }
