a74599b98c7bf8d1a9881ca7a82a2a0334e6d75e
Oleksandr Kirpachov <o.kirpachov@opinioni.net>
2024-07-29 22:23:14 +0200

    NEW showing public messages in landing page between sections

--- src/core/components/public-message/public-message.component.ts
+++ src/core/components/public-message/public-message.component.ts
@@ -1,5 +1,8 @@
-import {Component, Input} from '@angular/core';
+import {Component, computed, inject, Input, signal, WritableSignal} from '@angular/core';
 import {PublicMessageLocation} from "@core/lib/interfaces/public-message";
+import {PublicPagesDataService} from "@core/services/http/public-pages-data.service";
+import {TuiExpandModule} from "@taiga-ui/core";
+import {MatIcon} from "@angular/material/icon";
 
 export type PublicMessages = Record<string, string | null>;
 
@@ -12,10 +15,30 @@ export type PublicMessages = Record<string, string | null>;
 @Component({
   selector: 'app-public-message',
   standalone: true,
-  imports: [],
+  imports: [
+    TuiExpandModule,
+    MatIcon
+  ],
   templateUrl: './public-message.component.html',
   styleUrl: './public-message.component.scss'
 })
 export class PublicMessageComponent {
-  @Input({required: true}) location!: PublicMessageLocation;
+  private readonly service: PublicPagesDataService = inject(PublicPagesDataService);
+  readonly location$: WritableSignal<PublicMessageLocation | null> = signal(null);
+
+  @Input({required: true}) set location(location: PublicMessageLocation) {
+    this.location$.set(location);
+  }
+
+  get location(): PublicMessageLocation | null {
+    return this.location$();
+  }
+
+  readonly message = computed(() => {
+    const location: PublicMessageLocation | null = this.location$();
+    const messages: PublicMessages | null = this.service.messages();
+    if (location === null || messages === null) return null;
+
+    return messages[location];
+  })
 }
