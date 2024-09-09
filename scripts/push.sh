b099ca898cb7b63adb6344abe419dad7009c7f4b
Oleksandr Kirpachov <olecsandr.kirpachov@gmail.com>
2024-09-09 16:19:42 +0200

    updated build folder name

--- scripts/push.sh
+++ scripts/push.sh
@@ -3,18 +3,18 @@
 # Script should be run from the root of the project.
 
 # Checking if the build folder exists
-if [ ! -d "../build-angular-lpda2" ]; then
-  echo "The build folder does not exist. Expected to find it at '../build-angular-lpda2'."
+if [ ! -d "../lpda2-angular-build" ]; then
+  echo "The build repository folder does not exist. Expected to find it at '../lpda2-angular-build'."
   exit 1
 fi
 
 echo "Clearing output folder..."
-rm -rf ../build-angular-lpda2/*
+rm -rf ../lpda2-angular-build/*
 
 echo "Copying the build files..." && \
-  cd dist/lpda2 && cp -r * ../../../build-angular-lpda2/ && \
+  cd dist/lpda2 && cp -r * ../../../lpda2-angular-build/ && \
   echo "Pushing the changes to the repository..." && \
-  cd ../../../build-angular-lpda2 && \
+  cd ../../../lpda2-angular-build && \
   git add . && \
   git commit -m "update build" && \
   git push origin master && \
