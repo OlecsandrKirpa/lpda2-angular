c95b1e1393e2fbe3f34cf754ac7e07371cf17a04
Oleksandr Kirpachov <olecsandr.kirpachov@gmail.com>
2024-10-09 21:29:31 +0200

    FIX dont remove html tags if any

--- scripts/xlf-translate.rb
+++ scripts/xlf-translate.rb
@@ -97,7 +97,7 @@ original_file.search("trans-unit").each_with_index do |orig_trans, index|
     out_target = out_trans.search("target").first
     if original_source.to_s.gsub(/\s+/, " ").strip == trans_source.to_s.gsub(/\s+/, " ").strip && out_target && out_target.text.length > 0
       @report[:skipped_present] << { id: trans_id, source: original_source, target: out_target.text }
-      translated_str = out_target.text
+      translated_str = out_target.children.to_s
     end
   end
 
