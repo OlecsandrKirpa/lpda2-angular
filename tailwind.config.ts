a866b6b1ba535aaf6d52981ef83ef3e67920d827
Oleksandr Kirpachov <olecsandr.kirpachov@gmail.com>
2024-11-14 18:50:53 +0100

    NEW menu stepper + tailwind customization

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,20 @@ export default {
     './src/**/*.{html,ts}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--first-color)",
+        secondary: "var(--second-color)",
+        
+        // https://tailwindcss.com/docs/customizing-colors
+        success: "#22c55e", // green-500
+        "success-dark": "#15803d", // green-700
+        warning: "#eab308", // yellow-500
+        danger: "#ef4444", // red-500
+        info: "#3b82f6", // blue-500
+        // secondary: "#6b7280", // gray-500
+      },
+    },
   },
   plugins: [],
 } satisfies Config
