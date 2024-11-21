02023bdfc18b23bf0529656ba5bd8fe4c39a9782
Oleksandr Kirpachov <olecsandr.kirpachov@gmail.com>
2024-11-21 01:37:21 +0100

    NEW updated translations

--- scripts/translate.sh
+++ scripts/translate.sh
@@ -4,4 +4,6 @@ ng extract-i18n && \
   ruby scripts/format-xml.rb locales/messages.xlf && \
   ruby scripts/xlf-translate.rb locales/messages.xlf locales/messages.en.xlf && \
   ruby scripts/format-xml.rb locales/messages.en.xlf && \
-  ruby scripts/translations-check-interpolations.rb locales/messages.en.xlf
+  ruby scripts/translations-check-interpolations.rb locales/messages.en.xlf && 
+  ruby scripts/format-xml.rb locales/messages.xlf && \
+  ruby scripts/format-xml.rb locales/messages.en.xlf
